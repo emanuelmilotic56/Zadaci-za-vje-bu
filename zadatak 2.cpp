a58bc02b775b654815f238ce8d592fc1132093c2
emanuelmilotic56 <58472080+emanuelmilotic56@users.noreply.github.com>
2019-12-13 13:08:15 +0100

    Add files via upload

---
+++ zadatak 2.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+
+
+for(int i=0; i<=4; i++){
+	for(int b=0; b<=9; b++){
+		
+		cout<<i<<"-"<<b<<endl;
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+}
+
+
+
+
+
+cout<<"5-0"<<endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	system("Pause");
+	return 0;
+
+}
---
+++ zadatak 3.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+
+	
+int i;
+cout<<"[";
+
+for(i=4;i<22; i++){
+	 if(i%2==0){
+	 	
+	 cout<<i<<",";	
+	 	
+	 	
+	 	
+	 	
+	 	
+	 	
+	 	
+	 }
+	
+	
+}
+
+cout<<"]";
+	
+	
+	
+	system("Pause");
+	return 0;
+}
